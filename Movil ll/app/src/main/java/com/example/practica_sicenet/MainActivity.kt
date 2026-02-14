5df7e7cfea30bdd3be5ac564cd96f99ec2dc31de
Bismk11 <s22120167@alumnos.itsur.edu.mx>
2026-02-13 22:04:06 -0600

    ajustes

--- Movil ll/app/src/main/java/com/example/practica_sicenet/MainActivity.kt
+++ Movil ll/app/src/main/java/com/example/practica_sicenet/MainActivity.kt
@@ -4,29 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.practica_sicenet.ui.SicenetUiState
 import com.example.practica_sicenet.ui.SicenetViewModel
 import com.example.practica_sicenet.ui.theme.Practica_SicenetTheme
 import com.example.practica_sicenet.ui.screens.LoginScreen
 import com.example.practica_sicenet.ui.screens.ProfileScreen
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
